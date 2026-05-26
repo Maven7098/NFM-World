8e7e1b5cd7c8bc877cc4441f2372403708f2156d
Seongwhong <seongwhong@example.com>
2026-05-26 10:43:13 -0400

    Parameters now set as arguments

--- TrainingApp/TrainingProgram.cs
+++ TrainingApp/TrainingProgram.cs
@@ -9,12 +9,15 @@ namespace NFMWorld.MadEngine.AI
         static void Main(string[] args)
         {
             Console.WriteLine($"TelemetryPacket Size: {Marshal.SizeOf<TelemetryPacket>()}");
-            
-            // Set your parameters here
+
+            // Default parameters
             string ip = "127.0.0.1";
             int port = 9000;
-            string car = "nfmm/radicalone";
-            string stage = "nfm1/2_contrary";
+            string car = args.Length > 0 ? args[0] : "nfmm/radicalone";
+            string stage = args.Length > 1 ? args[1] : "nfm1/3_snakedance";
+
+            Console.WriteLine($"Training Car: {car}");
+            Console.WriteLine($"Training Stage: {stage}");
 
             var runner = new TrainingRunner();
             runner.StartTraining(ip, port, car, stage);
